08c2d947130014a8f3ca0d65db94d522bb9e03c2
Ali Gholami <aligholami7596@gmail.com>
2018-02-28 12:43:24 +0330

    Plots the scatter of Gaussian distribution

--- docs/assignment-1/src/linear-map/map.py
+++ docs/assignment-1/src/linear-map/map.py
@@ -10,6 +10,9 @@
     Implementation of a linear transformation on a multivariate Gaussian distribution.
 """
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import random
 
 MEAN_VECTOR = np.array([
     0,0
@@ -23,5 +26,12 @@ print("Mean Vector Shape: ", MEAN_VECTOR.shape)
 print("Cov Matrix Shape: ", COV_MATRIX.shape)
 
 GAUSSIAN_SAMPLES = np.random.multivariate_normal(MEAN_VECTOR, COV_MATRIX, size=500)
+X_SEQUENCE = np.random.normal(size=500)
 
-print(GAUSSIAN_SAMPLES)
+FIG = plt.figure()
+AX = Axes3D(FIG)
+
+AX.scatter(X_SEQUENCE, GAUSSIAN_SAMPLES[:, 0], GAUSSIAN_SAMPLES[:, 1])
+plt.show()
+
+#print(GAUSSIAN_SAMPLES)
