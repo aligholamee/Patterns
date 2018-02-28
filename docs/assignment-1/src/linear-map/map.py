f88b6179c2e452108fbaf53a72b210e304f85c48
Ali Gholami <aligholami7596@gmail.com>
2018-02-28 12:27:37 +0330

    Generates multivariate Gaussian samples

--- docs/assignment-1/src/linear-map/map.py
+++ docs/assignment-1/src/linear-map/map.py
@@ -9,3 +9,19 @@
 """
     Implementation of a linear transformation on a multivariate Gaussian distribution.
 """
+import numpy as np
+
+MEAN_VECTOR = np.array([
+    0,0
+])
+COV_MATRIX = np.array([
+    [1, 0],
+    [0, 1]
+])
+
+print("Mean Vector Shape: ", MEAN_VECTOR.shape)
+print("Cov Matrix Shape: ", COV_MATRIX.shape)
+
+GAUSSIAN_SAMPLES = np.random.multivariate_normal(MEAN_VECTOR, COV_MATRIX, size=500)
+
+print(GAUSSIAN_SAMPLES)
