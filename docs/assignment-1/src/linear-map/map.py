b63f7fdd30a2ca439255847420d942086869e15d
Ali Gholami <aligholami7596@gmail.com>
2018-02-28 15:39:57 +0330

    Managed to display the Gaussian plot

--- docs/assignment-1/src/linear-map/map.py
+++ docs/assignment-1/src/linear-map/map.py
@@ -13,27 +13,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from scipy.stats import multivariate_normal
 
-MEAN_VECTOR = np.array([
-    0,0
-])
+MEAN_VECTOR = np.array([0,0])
+
 COV_MATRIX = np.array([
     [1, 0],
     [0, 1]
 ])
 
-print("Mean Vector Shape: ", MEAN_VECTOR.shape)
-print("Cov Matrix Shape: ", COV_MATRIX.shape)
-
 GAUSSIAN_SAMPLES = np.random.multivariate_normal(MEAN_VECTOR, COV_MATRIX, size=500)
-X_SEQUENCE = np.random.normal(size=500)
+X = GAUSSIAN_SAMPLES[:, 0]
+Y = GAUSSIAN_SAMPLES[:, 1]
+X, Y = np.meshgrid(X, Y)
+XY = np.column_stack([X.flat, Y.flat])
+
+mu = [0, 0]
+cov = [
+    [1, 0],
+    [0, 1]
+]
+
+Z = multivariate_normal.pdf(XY, mean=mu, cov=cov)
+Z = Z.reshape(X.shape)
 
 FIG = plt.figure()
 AX = Axes3D(FIG)
 
-SURF = AX.plot_trisurf(X_SEQUENCE, GAUSSIAN_SAMPLES[:, 0], GAUSSIAN_SAMPLES[:, 1], cmap=cm.jet, linewidth=0.1)
+SURF = AX.plot_surface(X, Y, Z, cmap=cm.winter)
 FIG.colorbar(SURF, shrink=0.5, aspect=5)
 
 plt.show()
 
-#print(GAUSSIAN_SAMPLES)
