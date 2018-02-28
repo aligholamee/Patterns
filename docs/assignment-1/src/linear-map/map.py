21956f004d30606f73228ecae317beafe61caebe
Ali Gholami <aligholami7596@gmail.com>
2018-02-28 12:04:32 +0330

    Linear map base was added

---
+++ docs/assignment-1/src/linear-map/map.py
@@ -0,0 +1,11 @@
+# ========================================
+# [] File Name : map.py
+#
+# [] Creation Date : February 2018
+#
+# [] Created By : Ali Gholami (aligholami7596@gmail.com)
+# ========================================
+
+"""
+    Implementation of a linear transformation on a multivariate Gaussian distribution.
+"""
