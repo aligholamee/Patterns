4c7e4c9d18476d575b50c7145c077f80ab0ee266
Ali Gholami <aligholami7596@gmail.com>
2018-02-28 16:00:41 +0330

    Gonna add a linear transformation soon :)

--- docs/assignment-1/src/linear-map/map.py
+++ docs/assignment-1/src/linear-map/map.py
@@ -22,15 +22,20 @@ COV_MATRIX = np.array([
     [0, 1]
 ])
 
+# Generate Gaussian samples
 GAUSSIAN_SAMPLES = np.random.multivariate_normal(MEAN_VECTOR, COV_MATRIX, size=500)
 X = GAUSSIAN_SAMPLES[:, 0]
 Y = GAUSSIAN_SAMPLES[:, 1]
 X, Y = np.meshgrid(X, Y)
 XY = np.column_stack([X.flat, Y.flat])
 
+# Use multivariate normal as the result for Z
 Z = multivariate_normal.pdf(XY, mean=MEAN_VECTOR.tolist(), cov=COV_MATRIX.tolist())
 Z = Z.reshape(X.shape)
 
+# Linear transformation
+
+# Plot
 FIG = plt.figure()
 AX = Axes3D(FIG)
 
@@ -38,4 +43,5 @@ SURF = AX.plot_surface(X, Y, Z, cmap=cm.winter)
 FIG.colorbar(SURF, shrink=0.5, aspect=5)
 
 plt.show()
+
 
