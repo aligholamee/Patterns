78c77c6aff34f9f739596e066db77a178869c020
Ali Gholami <aligholami7596@gmail.com>
2018-03-15 16:56:21 +0330

    Added normal distribution discriminators

---
+++ docs/assignment-2/src/1.c/plot.py
@@ -0,0 +1,46 @@
+import pylab as pl
+import numpy as np
+
+D = 2
+
+# Different Mean Vectors
+M1 = np.array([0.0, 2.0])
+M2 = np.array([3.0, 1.0])
+M3 = np.array([1.0, 0.0])
+
+# Same Covariance Matrix
+C = np.array([
+    [1.0, 0.0],
+    [0.0, 1/3]
+])
+
+X, Y = np.mgrid[-2:2:100j, -2:2:100j]
+points = np.c_[X.ravel(), Y.ravel()]
+
+invC = np.linalg.inv(C)
+
+v = points - M1
+g1 = -0.5*np.sum(np.dot(v, invC) * v, axis=1) - D*0.5*np.log(2*np.pi) - 0.5*np.log(np.linalg.det(C))
+g1.shape = 100, 100
+
+v = points - M2
+g2 = -0.5*np.sum(np.dot(v, invC) * v, axis=1) - D*0.5*np.log(2*np.pi) - 0.5*np.log(np.linalg.det(C))
+g2.shape = 100, 100
+
+v = points - M3
+g3 = -0.5*np.sum(np.dot(v, invC) * v, axis=1) - D*0.5*np.log(2*np.pi) - 0.5*np.log(np.linalg.det(C))
+g3.shape = 100, 100
+
+fig, axes = pl.subplots(1, 6, figsize=(15, 5))
+ax1, ax2, ax3, ax4, ax5, ax6 = axes.ravel()
+for ax in axes.ravel():
+    ax.set_aspect("equal")
+
+ax1.pcolormesh(X, Y, g1)
+ax2.pcolormesh(X, Y, g2)
+ax3.pcolormesh(X, Y, g3)
+ax4.pcolormesh(X, Y, g1 > g2)
+ax5.pcolormesh(X, Y, g2 > g3)
+ax6.pcolormesh(X, Y, g1 > g3)
+
+pl.show()
