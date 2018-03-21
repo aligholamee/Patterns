f9de0bf2e4a8ad711fee45ad6a2fb3e43021a053
Ali Gholami <aligholami7596@gmail.com>
2018-03-21 18:08:50 +0330

    Boundary to be added

--- docs/assignment-2/src/6/plot.py
+++ docs/assignment-2/src/6/plot.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 class_1 = np.array([
     [1.5, 0],
@@ -38,14 +37,13 @@ print("Class 2 mean: ", meu_2)
 print("Class 2 covariance: ", cov_2)
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(class_1[:, 0], class_1[:, 1], color='darkblue')
-ax.scatter(class_2[:, 0], class_2[:, 1], color='red')
-
-# Customize the figure background color
-plt.gca().patch.set_facecolor('white')
-ax.w_xaxis.set_pane_color((0.8, 0.8, 0.8, 1.0))
-ax.w_yaxis.set_pane_color((0.8, 0.8, 0.8, 1.0))
-ax.w_zaxis.set_pane_color((0.8, 0.8, 0.8, 1.0))
+ax = fig.add_subplot(111)
+ax.scatter(class_1[:, 0], class_1[:, 1], marker='o', color='darkblue')
+ax.scatter(class_2[:, 0], class_2[:, 1], marker='^', color='red')
+plt.legend(['Class1 (w1)', 'Class2 (w2)'], loc='upper left') 
+plt.xlabel('x1')
+plt.ylabel('x2')
+ftext = 'p(x|c1) ~ N(mu1=[1.571,1.571], cov1=[[0.452, 0.119], [0.119, 0.952]])\np(x|c2) ~ N(mu2=[-0.15, -0.15], cov2=[[1.725, 0.002], [0.002, 0.558]])'
+plt.figtext(.9,.9, ftext, fontsize=10, ha='right')
 plt.show()
 
