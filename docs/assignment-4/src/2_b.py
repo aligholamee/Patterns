10d94f1168bb7b5b39a15efdc05ca4f8d31ce0f9
Ali Gholami <aligholami7596@gmail.com>
2018-05-19 22:04:35 +0430

    Completed Q3

--- docs/assignment-4/src/2_b.py
+++
@@ -1,7 +0,0 @@
-import math
-
-
-dataset = [2, 3, 4, 4, 4, 5, 5, 10, 11, 11, 11, 12, 14, 16]
-
-
-# Define the 
---
+++ docs/assignment-4/src/knn_plot.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors
+
+n_neighbors = 1
+
+
+X = np.array([
+    [2, 4],
+    [4, 2],
+    [4, 6],
+    [6, 4],
+    [4, 4],
+    [6, 2]
+])
+
+y = np.array([
+    [0],
+    [0],
+    [0],
+    [0],
+    [1],
+    [1]
+])
+
+h = .02  # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], cmap=cmap_bold)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("2-Class classification (k = %i, weights = '%s')"
+              % (n_neighbors, weights))
+
+plt.show()
