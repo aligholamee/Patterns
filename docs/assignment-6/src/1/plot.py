906d922fa903c0c8d22302b072b05531ad9bb77a
Ali Gholami <aligholami7596@gmail.com>
2018-06-29 11:37:03 +0430

    Minor bug fixes

--- docs/assignment-6/src/1/plot.py
+++ docs/assignment-6/src/1/plot.py
@@ -16,7 +16,7 @@ weights = np.array([
     [1, -4, 3]
 ]) 
 
-x = np.linspace(-5, 5, 1000)
+x = np.linspace(-4, 4, 1000)
 
 
 # Plot points
@@ -30,13 +30,19 @@ class_2 = np.array([
     [-3, -1]
 ])
 
-plt.scatter(class_1[:, 0], class_1[0, :], marker='^', label='Class 1', c='black')
-plt.scatter(class_2[:, 0], class_2[0, :], marker='o', label='Class 2', c='black')
+
+plt.scatter(class_1[:, 0], class_1[0, :], marker='o', label='Class 1', c='darkred')
+
+plt.scatter(2, -1, marker='o', label='Class 2', c='darkblue')
+plt.scatter(-3, -1, marker='o', c='darkblue')
+# plt.scatter(class_2[:, 0], class_2[0, :], marker='o', label='Class 2', c='darkblue')
 
 # Plot decision lines
-for i in range(5):
+for i in range(0, 5):
     lab = 'y = ' + str(weights[i][1]) + 'x1 + ' + str(weights[i][2]) + 'x2 + ' + str(weights[i][0])
-    plt.plot(x, (-1 * weights[i][1] * x + weights[i][0] * x) / (weights[i][2]), colors[i], label=lab)
+    plt.plot(x, (-1 * weights[i][1] * x + weights[i][0]) / (weights[i][2]), colors[i], label=lab)
 
 plt.legend(loc='best')
+plt.xlim(-6, 6)
+plt.ylim(-6, 6)
 plt.show()
