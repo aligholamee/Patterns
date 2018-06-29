189ceade3a0e5c866b99141017006e74d3e9445a
Ali Gholami <aligholami7596@gmail.com>
2018-06-29 22:20:28 +0430

    Update: completed or problem

--- docs/assignment-6/src/2/percept.py
+++ docs/assignment-6/src/2/percept.py
@@ -5,11 +5,25 @@ import random
 num_epoch = 50
 learning_rate = 0.1
 
+aug_norm_or_d = np.array([
+    [-1, 0, 0, 0],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 1]
+])
+
 or_d = np.array([
     [0, 0, 0],
     [0, 1, 1],
     [1, 0, 1],
-    [1, 1, 1]
+    [1, 1, 1] 
+])
+
+aug_norm_xor_d = np.array([
+    [-1, 0, 0, 0],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [-1, -1, -1, 0]
 ])
 
 xor_d = np.array([
@@ -22,18 +36,22 @@ xor_d = np.array([
 # Generate random weights
 weights = np.array([random.uniform(0, 1) for i in range(3)]).reshape(1, 3)
 
-def relu()
 for i in range(num_epoch):
     # Load the whole dataset
-    for (x, y, l) in np.ndenumerate(dataset):
+    for (b, x, y, l) in np.ndenumerate(aug_norm_or_d):
 
         # Classify the loaded point using weights in step i - 1
-        output = relu(weights[1] * x + weights[2] * y + weights[0])
-        if(output != l):
+        output = weights[1] * x + weights[2] * y + weights[0] * b
+        if(output <= 0):
             # Update the weights
-            w[0] = 
-            w[1] = 
-            w[2] = 
+            weights[0] = weights[0] + learning_rate * b
+            weights[1] = weights[1] + learning_rate * x
+            weights[2] = weights[2] + learning_rate * y
+        else:
+            weights[0] = weights[0]
+            weights[1] = weights[1]
+            weights[2] = weights[2]
+        
 
 
 
