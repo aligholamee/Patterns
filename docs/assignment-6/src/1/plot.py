b7bb6b45174fc911f24599373f044799a69ce494
Ali Gholami <aligholami7596@gmail.com>
2018-06-29 10:59:38 +0430

    Added class points

--- docs/assignment-6/src/1/plot.py
+++ docs/assignment-6/src/1/plot.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+colors = ['blue', 'red', 'green', 'cyan', 'purple']
 
 def plotLines(w0, w1, w2):
     d_x = np.linspace(-5, 5, 1000)
@@ -15,7 +18,22 @@ weights = np.array([
 
 x = np.linspace(-5, 5, 1000)
 
-for i in range(5):
-    plt.plot(x, (-1 * weights[1] * x + weights[0] * x) / (weights[2] * x))
 
+# Plot points
+class_1 = np.array([
+    [1, 1],
+    [-3, 1]
+])
+
+class_2 = np.array([
+    [2, -1],
+    [-3, -1]
+])
+
+# Plot decision lines
+for i in range(5):
+    lab = 'y = ' + str(weights[i][1]) + 'x1 + ' + str(weights[i][2]) + 'x2 + ' + str(weights[i][0])
+    plt.plot(x, (-1 * weights[i][1] * x + weights[i][0] * x) / (weights[i][2]), colors[i], label=lab)
+
+plt.legend(loc='bottm center')
 plt.show()
